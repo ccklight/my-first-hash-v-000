82acea0aea81c588b50bc009ef7c0bd6a60f3dcf
Cheryl K <cherylkelson@icloud.com>
2018-09-19 20:27:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,13 +28,13 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-shipping_manifest = [ "muskets" => 2, "gun powder" => 4]
+# shipping_manifest = [ "muskets" => 2, "gun powder" => 4]
 
-shipping_manifest["gun powder"] = 4
-shipping_manifest["muskets"] = 2
+# shipping_manifest["gun powder"] = 4
+# shipping_manifest["muskets"] = 2
 
-shipping_manifest = {:item1 => 1}
-shipping_manifest[:item2] = 2
+# shipping_manifest = {:item1 => 1}
+# shipping_manifest[:item2] = 2
 
 
 # shipping_manifest["gun powder" => 4, "muskets" => 2]
