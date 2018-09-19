c4b996910d207ffd8c4f460f895c454b36589e13
Cheryl K <cherylkelson@icloud.com>
2018-09-19 19:53:07 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-desserts = {"french_pastry" => "macaron", "italian_pastry" => "cannoli", 
-  
+International_ desserts = {"French_pastry" => "macaron", "Italian_pastry" => "cannoli", "German_pastry" => "streusel" 
+}
 end
 
 
