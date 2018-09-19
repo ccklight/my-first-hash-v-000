e2753fd11f7043e13626cbff1060fa23d93a01a4
Cheryl K <cherylkelson@icloud.com>
2018-09-19 20:17:44 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,11 +30,11 @@ def adding
                  }
 shipping_manifest = [ "muskets" => 2, "gun powder" => 4]
 
-# shipping_manifest["gun powder"] = 4
-#shipping_manifest["muskets"] = 2
+shipping_manifest["gun powder"] = 4
+shipping_manifest["muskets"] = 2
 
-shipping_manifest["gun powder" => 4, "muskets" => 2]
-shipping_manifest
+# shipping_manifest["gun powder" => 4, "muskets" => 2]
+# shipping_manifest
 
 
 
